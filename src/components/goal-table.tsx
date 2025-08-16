288bf8958b919a6c3cb49f3f3590ce80dd959b49
Jordan Murray <jordan102@gmail.com>
2025-08-16 02:11:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/goal-table.tsx
+++ src/components/goal-table.tsx
@@ -100,7 +100,7 @@ export function GoalTable() {
 
   const renderLoadingSkeleton = () => (
     Array.from({ length: 2 }).map((_, i) => (
-      <TableRow key={'skeleton-goal-${i}'}>
+      <TableRow key={`skeleton-goal-${i}`}>
         <TableCell colSpan={3}><Skeleton className="h-10 w-full" /></TableCell>
       </TableRow>
     ))
