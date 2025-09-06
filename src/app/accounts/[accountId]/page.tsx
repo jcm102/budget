cef896ab972a28b312b56f0dcf26bbedaf0da341
Jordan Murray <jordan102@gmail.com>
2025-09-06 21:28:37 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/accounts/[accountId]/page.tsx
+++ src/app/accounts/[accountId]/page.tsx
@@ -28,8 +28,7 @@ import {
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
-export default function AccountDetailPage({ params }: { params: { accountId: string } }) {
-  const accountId = params.accountId;
+export default function AccountDetailPage({ params: { accountId } }: { params: { accountId: string } }) {
   const { accounts, isLoading: isLoadingAccounts } = useAccountDetails();
   const { 
     transactions, 
