862108e88cbf13f3a08f015674c1eb942ff89a6f
Jordan Murray <jordan102@gmail.com>
2025-10-06 21:17:11 +0000

    it hasn’t fixed there error it still cannot load from the database

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,9 @@
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   "projectId": "tasktrack-budget",
   "appId": "1:1046918211235:web:ff0666313439436b7fc116",
-  "storageBucket": "tasktrack-budget.firebasestorage.app",
   "apiKey": "AIzaSyD2Lqb1HoYVNUjD1LC6HUhRk-Xy1zL3jec",
   "authDomain": "tasktrack-budget.firebaseapp.com",
   "measurementId": "",
@@ -16,7 +15,7 @@ let app;
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
 } else {
-  app = getApps()[0];
+  app = getApp();
 }
 
 const db = getFirestore(app);
