d3a4d2dd2b53f074961e18c60f13ad200da57baf
Jordan Murray <jordan102@gmail.com>
2025-10-06 20:29:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "tasktrack-budget",
+  "appId": "1:1046918211235:web:ff0666313439436b7fc116",
+  "apiKey": "AIzaSyD2Lqb1HoYVNUjD1LC6HUhRk-Xy1zL3jec",
+  "authDomain": "tasktrack-budget.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1046918211235"
+};
