d1152e241b8fe2493741b807869fba9e6337ecd6
Jordan Murray <jordan102@gmail.com>
2025-10-07 13:43:56 +0000

    Try fixing this error: `Console Error: FirebaseError: Missing or insuffi

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState, useEffect } from 'react';
@@ -85,23 +86,26 @@ export function useCollection<T = any>(
         setIsLoading(false);
       },
       (error: FirestoreError) => {
+        let contextualError: Error = error;
         // This logic extracts the path from either a ref or a query
-        const path: string =
-          memoizedTargetRefOrQuery.type === 'collection'
-            ? (memoizedTargetRefOrQuery as CollectionReference).path
-            : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
+        if (error.code === 'permission-denied') {
+          const path: string =
+            memoizedTargetRefOrQuery.type === 'collection'
+              ? (memoizedTargetRefOrQuery as CollectionReference).path
+              : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
 
-        const contextualError = new FirestorePermissionError({
-          operation: 'list',
-          path,
-        })
+          contextualError = new FirestorePermissionError({
+            operation: 'list',
+            path,
+          });
+
+          // trigger global error propagation
+          errorEmitter.emit('permission-error', contextualError as FirestorePermissionError);
+        }
 
         setError(contextualError)
         setData(null)
         setIsLoading(false)
-
-        // trigger global error propagation
-        errorEmitter.emit('permission-error', contextualError);
       }
     );
 
