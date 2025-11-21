c50fe0219a2bbbc6e000c37c89144e576574a6ca
Jordan Murray <jordan102@gmail.com>
2025-11-21 17:52:03 +0000

    can we also fix the cards, budgeted income takes the whole screen and th

--- src/app/monthly-budget/page.tsx
+++ src/app/monthly-budget/page.tsx
@@ -177,7 +177,7 @@ export default function MonthlyBudgetPage() {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                 <Link href="/budget" className="md:col-span-2 lg:col-span-1">
+                 <Link href="/budget" className="block">
                     <div 
                     className="p-4 border rounded-lg bg-card cursor-pointer hover:bg-accent transition-colors h-full"
                     >
