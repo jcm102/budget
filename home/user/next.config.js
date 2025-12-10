8cc3e88ee5b790dcab9cdd080c549fdc280cb2e0
Jordan Murray <jordan102@gmail.com>
2025-12-10 02:59:10 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

---
+++ home/user/next.config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
