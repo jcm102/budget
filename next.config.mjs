46cb95c40410a08e94823252833fba324a594921
Jordan Murray <jordan102@gmail.com>
2025-12-22 01:16:27 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  serverActions: {
-    bodySizeLimit: '4.5mb',
+  webpack(config) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    return config;
   },
 };
 
